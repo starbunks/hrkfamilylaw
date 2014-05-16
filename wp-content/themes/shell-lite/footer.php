a57936f1b49ac55690afd144096402d0ee4f2667
Brendan Gualdoni <gualdoni@gmail.com>
2014-05-16 12:18:45 -0500

    new footer

--- wp-content/themes/shell-lite/footer.php
+++ wp-content/themes/shell-lite/footer.php
@@ -1,36 +1,73 @@
 <?php
-/**
- * Theme's Footer
- *
- *
- * @file           footer.php
- * @package        WordPress 
- * @subpackage     Shell 
- * @author         Emil Uzelac 
- * @copyright      2003 - 2012 ThemeID
- * @license        license.txt
- * @version        Release: 1.0
- * @filesource     wp-content/themes/shell-lite/footer.php
- * @link           http://codex.wordpress.org/Theme_Development#Footer_.28footer.php.29
- * @since          available since Release 1.0
- */
+
+if (!is_page(249)) {
+
 ?>
+
     </div><!-- end of #wrapper -->
     <?php shell_wrapper_end(); // after wrapper hook ?>
 </div><!-- end of #container -->
 <?php shell_container_end(); // after container hook ?>
 
-<div id="footer">
-    <div id="footer-wrapper">
-        <div class="grid col-300 copyright">
-            <?php esc_attr_e('&copy;', 'shell'); ?> <?php _e(date('Y')); ?><a href="<?php echo home_url('/') ?>" title="<?php echo esc_attr(get_bloginfo('name', 'display')); ?>">
-                <?php bloginfo('name'); ?>
-            </a>
-        </div><!-- end of .copyright -->
-        
-    </div><!-- end #footer-wrapper -->
-</div><!-- end #footer -->
+<?php } ?>
 
+<div class="footerBackground clearfix">
+<div class="footerContent">
+	<div class="row">
+	<div class="col-sm-1"></div>
+	<div class="col-sm-3 recentPosts"><h2>Recent Posts</h2>
+	<div class="clearfix">
+<?php
+
+$args = array(
+    'numberposts' => 6,
+    'offset' => 0,
+    'category' => 0,
+    'orderby' => 'post_date',
+    'order' => 'DESC',
+    'post_type' => 'post',
+    'post_status' => 'draft, publish',
+    'suppress_filters' => true );
+
+	$recent_posts = wp_get_recent_posts( $args );
+	foreach( $recent_posts as $recent ){
+		echo '<div class="clearfix"><div class="marker"></div><div class="linkItem"><a href="' . get_permalink($recent["ID"]) . '" title="Look '.esc_attr($recent["post_title"]).'" >' .   $recent["post_title"].'</a></div></div>';
+	}
+?>
+
+	</div>
+	
+	</div>
+	
+	<div class="col-sm-3 central">
+	<h2>Blog Topics</h2>
+	<?php
+		$catList = wp_list_categories("echo=0&hide_empty=0&title_li=&exclude=1");
+		$catList = strip_tags($catList,"<a>");
+		$catList = str_replace("<a",'<div class="catLink"><a',$catList);
+		$catList = str_replace("</a>","</a></div>",$catList);
+			echo $catList;
+		?>
+		<div><a class="btn btn-primary pull-left" href="<?php bloginfo('url'); ?>/feed/">Subscribe via RSS</a></div>
+	</div>
+<div class="col-sm-3 contactUs"><h2>Contact Us</h2>
+	<p>	30 N. LaSalle Street<br/>Suite 1210<br/>
+		Chicago, IL 60602
+	</p>
+	<p>	
+		Phone: 312.782.2400
+	</p>
+	<p>	
+		Fax: 312.782.4025
+	</p>
+	<p>
+		Email: <a class="footerAnchor" href="mailto:info@hrkfamilylaw.com">info@hrkfamilylaw.com</a>
+	</p>
+	</div>
+	<div class="col-sm-1"></div>
+	
+	</div> <!-- /.row in footer -->
+</div> <!-- /.footerContent -->
+
+</div> <!-- /.footerBackground -->
 <?php wp_footer(); ?>
-</body>
-</html>
